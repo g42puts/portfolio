eff092baeb557ad751ce4b0a523c7b5f85b772d0
g42puts <gilmar.neo@gmail.com>
2024-03-27 11:11:41 -0300

    feat: added description

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,8 @@ import Layout from "components/Layout/Layout";
 function App() {
   const currentUrl = window.location.toString();
   const title = "Gilmar's Portfólio";
+  const description = "Portfólio do Dev Full Stack Jr., Gilmar José."
+
   return (
     <>
       <Helmet
@@ -24,7 +26,7 @@ function App() {
         <html lang="pt-br" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Portfólio de Gilmar" />
+        <meta name="description" content={description} />
         <meta name="keywords" content="portfólio, dev junior, desenvolvedor junior, junior developer, jr. developer, dev fullstack, dev full-stack, desenvolvedor fullstack, desenvolvedor full-stack, desenvolvedor full stack" />
         <meta name="robots" content="all" />
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -36,7 +38,7 @@ function App() {
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content={title} />
         <meta property="og:title" content={title} />
-        <meta property="og:description" />
+        <meta property="og:description" content={description} />
         {/* <meta property="og:image" /> */}
         {/* End Facebook Metadata */}
       </Helmet>
