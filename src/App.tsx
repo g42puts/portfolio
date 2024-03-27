e320b919f0eb920fe6c1f3c1aaf781da5fd3b918
g42puts <gilmar.neo@gmail.com>
2024-03-27 11:03:16 -0300

    feat/wip: included react-helmet and added new SEO configs

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,38 @@ import { Tools } from "libs/tools";
 import fotoPerfil from "/foto_perfil.jpg"
 import Layout from "components/Layout/Layout";
 
+import { Helmet } from "react-helmet";
+
 function App() {
+  const currentUrl = window.location.toString();
+  const title = "Gilmar's Portfólio";
   return (
     <>
+      <Helmet
+        defaultTitle={title}
+      >
+        {/* Html Metadata */}
+        <title>{title}</title>
+        <body className="root" />
+        <html lang="pt-br" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Portfólio de Gilmar" />
+        <meta name="keywords" content="portfólio, dev junior, desenvolvedor junior, junior developer, jr. developer, dev fullstack, dev full-stack, desenvolvedor fullstack, desenvolvedor full-stack, desenvolvedor full stack" />
+        <meta name="robots" content="all" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="canonical" href={currentUrl} />
+        {/* End Html Metadata */}
+
+        {/* Facebook Metadata */}        
+        <meta property="og:url" content={currentUrl}/>
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content={title} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" />
+        {/* <meta property="og:image" /> */}
+        {/* End Facebook Metadata */}
+      </Helmet>
       <Layout>
         <Introduction profileImg={fotoPerfil} />
         <main>
