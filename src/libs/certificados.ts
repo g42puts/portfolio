75761b96f449ad0f30d3c926bc5d878ad30bb641
g42puts <gilmar.neo@gmail.com>
2024-03-28 23:02:25 -0300

    feat/wip: added certificates and new feats

--- src/libs/certificados.ts
+++ src/libs/certificados.ts
@@ -1,3 +1,40 @@
-export const Certificados = {
-  
-}
+import AluraCPPIntro from "/certificados-prints/cpp-avancando-com-cpp-entenda-melhor-a-linguagem.png";
+import AluraCPPIntroESTL from "/certificados-prints/cpp-conhecendo-a-linguagem-e-a-stl.png";
+import AluraLua from "/certificados-prints/lua-do-zero-ao-jogo.png";
+import AluraDjangoAuthFormsAlerts from "/certificados-prints/django-autenticação-de-formularios-e-alerta.png";
+import AluraDjangoCrudS3 from "/certificados-prints/django-crud-e-persistencia-no-s3.png";
+import AluraDjangoPersisDataAdmin from "/certificados-prints/django-persistencia-de-dados-e-admin.png";
+import AluraDjangoBoasPraticas from "/certificados-prints/django-templates-e-boas-praticas.png";
+import AluraPythonTestes from "/certificados-prints/python-e-tdd-explorando-testes-unitarios.png";
+import AluraPythonParaData from "/certificados-prints/python-para-data-science.png";
+import AluraPythonParaDataNumpy from "/certificados-prints/python-para-data-science-linguagem-e-numpy.png";
+
+import BradescoPOOPython from "/certificados-prints/bradesco-desenvolvimento-orientado-a-objetos-linguagem-python.png";
+import BradescoLinguagemProg from "/certificados-prints/bradesco-linguagem-de-programacao-basico.png";
+
+import IBMDataScience101 from "/certificados-prints/data-science-101-ibm.png";
+import DioFundamentosNuvem from "/certificados-prints/dio-fundamentos-de-computacao-em-nuvem-na-aws.png";
+import FlaskWebAppPython from "/certificados-prints/flask-crie-uma-webapp-com-python.png";
+
+export const Certificados = [
+  { name: "Avançando com C++: Entenda melhor a linguagem", org: "Alura", img: AluraCPPIntro },
+  { name: "C++: Conhecendo a linguagem e a STL", org: "Alura", img: AluraCPPIntroESTL },
+
+  { name: "Lua: Do zero ao jogo", org: "Alura", img: AluraLua },
+
+  { name: "Django: Autenticação de formulários e alerta", org: "Alura", img: AluraDjangoAuthFormsAlerts },
+  { name: "Django: Crud e persistência no S3", org: "Alura", img: AluraDjangoCrudS3 },
+  { name: "Django: Persistência de dados e admin", org: "Alura", img: AluraDjangoPersisDataAdmin },
+  { name: "Django: Templates e boas práticas", org: "Alura", img: AluraDjangoBoasPraticas },
+
+  { name: "Python e TDD: Explorando testes unitários", org: "Alura", img: AluraPythonTestes },
+  { name: "Python para Data Science", org: "Alura", img: AluraPythonParaData },
+  { name: "Python para Data Science: Linguagem e Numpy", org: "Alura", img: AluraPythonParaDataNumpy },
+  { name: "Flask: Crie uma Webapp com Python", org: "Alura", img: FlaskWebAppPython },
+
+  { name: "Desenvolvimento orientado a objetos utilizando a linguagem Python", org: "Bradesco", img: BradescoPOOPython },
+  { name: "Linguagem de programação Python - Básico", org: "Bradesco", img: BradescoLinguagemProg },
+
+  { name: "Data Science 101", org: "IBM", img: IBMDataScience101 },
+  { name: "Fundamentos da Compotação em Nuvem na AWS", org: "DIO", img: DioFundamentosNuvem },
+];
